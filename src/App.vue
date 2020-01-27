57589ee03d02b059255dcedea1ea7af491c30e0a
yoonhong <mkonji23@naver.com>
2020-01-27 15:56:17 +0900

    화면 router-link 설정

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
 	<v-app>
-		<router-view />
+		<main-vue></main-vue>
 	</v-app>
 </template>
 
 <script>
+import MainVue from '@/views/MainVue.vue';
 export default {
+	components: { MainVue },
 	name: 'App',
-
 	data: () => ({
 		//
 	}),
